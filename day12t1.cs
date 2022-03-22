5f7ba6fce72446dfe1cb85f4df4395a7519b749f
surya .D <100852000+theeminent63@users.noreply.github.com>
2022-03-22 03:41:52 -0700

    Add files via upload

---
+++ day12t1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace ConsoleApp2
+{
+    class day12t1
+    {
+        public static void Main()
+        {
+
+            FileStream fs = new FileStream("C:\\softura\\training\\programs\\day12\\Deserts.txt", FileMode.OpenOrCreate, FileAccess.ReadWrite);
+            StreamWriter bw = new StreamWriter(fs);
+            List<string> str = new List<String>();
+            str.Add("Coconut Truffle");
+            str.Add("Rasamalai");
+            str.Add("Caramel Custard");
+            str.Add("Bread Halwa");
+            str.Add("Lichi Mousse");
+            foreach (string d in str)
+            {
+                bw.Write("\n" + d);
+            }
+            bw.Flush();
+            bw.Close();
+            fs.Close();
+            FileStream fs1 = new FileStream("C:\\softura\\training\\programs\\day12\\Desserts.txt", FileMode.OpenOrCreate, FileAccess.ReadWrite);
+            StreamReader sr = new StreamReader(fs1);
+            String s = sr.ReadToEnd();
+            sr.Close();
+            fs1.Close();
+            Console.WriteLine(s);
+            FileInfo fi = new FileInfo(@"C:\\softura\\training\\programs\\day12\\Desserts.txt");
+            Console.WriteLine(fi.Length);
+            Console.WriteLine(fi.CreationTime);
+            Console.Read();
+        }
+    }
+}
---
+++ day12t2.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace ConsoleApp2
+{
+    [Serializable]
+    class Employee
+    {
+        public String EmployeeID = "VBNM";
+        public String Name = "Ronaldo";
+        public String Gen = "Male";
+        public int age = 15;
+    }
+    class day12t2
+    {
+        public void SeritoFile()
+        {
+
+            Employee Eobj = new Employee();
+            FileStream fs = new FileStream("Details.txt", FileMode.OpenOrCreate, FileAccess.ReadWrite);
+            BinaryFormatter bf = new BinaryFormatter();
+            bf.Serialize(fs, Eobj);
+            fs.Close();
+        }
+        public void DesrialData()
+        {
+            FileStream fs = new FileStream("Details.txt", FileMode.OpenOrCreate, FileAccess.ReadWrite);
+            BinaryFormatter f = new BinaryFormatter();
+            Employee Ebj = (Employee)f.Deserialize(fs);
+            Console.WriteLine(Ebj.Name);
+            Console.WriteLine(Ebj.EmployeeID);
+            Console.WriteLine(Ebj.Gen);
+            Console.WriteLine(Ebj.age);
+        }
+
+
+        public static void Main()
+        {
+            day12t2 obj = new day12t2();
+            obj.SeritoFile();
+            obj.DesrialData();
+
+            Console.ReadLine();
+
+
+        }
+
+    }
+}
