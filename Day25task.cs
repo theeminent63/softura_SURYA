2860bbf9ec078f28a3e50fbea44835d1291f246e
surya .D <100852000+theeminent63@users.noreply.github.com>
2022-04-09 18:26:41 +0530

    Add files via upload

---
+++ Day25task.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lynq
+{
+    class Day25Task
+    {
+        public static void Main()
+        {
+            int Legs, Eyes, Animals, Goat, Duck, Tlegs, i;
+            Legs = 80;
+            Eyes = 52;
+            Animals = Eyes / 2;
+            for (i = 1; i <= Legs; i++)
+            {
+                Goat = i / 4;
+                Duck = (Legs - i) / 2;
+                Tlegs = (Goat * 4) + (Duck * 2);
+                if ((Goat + Duck) == Animals && Tlegs == Legs)
+                {
+                    Console.WriteLine("Goat=" + Goat + "\n" + "Duck=" + Duck);
+                }
+            }
+
+        }
+    }
+}
