99df0d4bdff45052fd57eff766858d6f2b566102
surya .D <100852000+theeminent63@users.noreply.github.com>
2022-04-28 03:19:27 -0700

    Add files via upload

---
+++ EF Product/Models/Product.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace NewEF.Models
+{
+    public class Product
+    {
+        [Key]
+        [Required]
+        public int ProductID { get; set; }
+        public string ProdName { get; set; }
+        public DateTime ManfDate { get; set; }
+    }
+}
---
+++ EF Product/Models/ProductDBContext.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore; 
+namespace NewEF.Models
+{
+    public class ProductDBContext : DbContext
+    {
+        public ProductDBContext()
+        {
+
+        }
+        public ProductDBContext(DbContextOptions<ProductDBContext> options) : base(options)
+        { 
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=DESKTOP-RRCVLCD;Initial Catalog=Trial;Integrated Security=True;Persist Security Info=False;Pooling=False;MultipleActiveResultSets=False;Encrypt=False;TrustServerCertificate=False");
+        }
+        public virtual DbSet<Product> Products { get; set; }
+    }
+}
