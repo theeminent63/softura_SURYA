27341b9a3b7b30ccf90192715f16f9283e574e2b
surya .D <100852000+theeminent63@users.noreply.github.com>
2022-04-28 03:37:51 -0700

    Add files via upload

---
+++ EF Product/Controllers/MovieController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using NewEF.Models;
+
+namespace NewEF.Controllers
+{
+    public class MovieController : Controller
+    {
+       
+        public IActionResult Indx()
+        {
+            return View(); 
+        }
+        [Route("Movies/Create")]
+        public IActionResult Create(Movie m)
+        {
+            if (ModelState.IsValid)
+            {
+                MovieDBContext dBContext = new MovieDBContext();
+                dBContext.Add(m);
+                dBContext.SaveChanges();
+                return Content("Saved");
+            }
+            return View("Index");
+            
+        }
+    }
+}
---
+++ EF Product/Controllers/ProductController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using NewEF.Models;
+
+namespace NewEF.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [Route("Product/prod")]
+        public IActionResult prod(Product pd)
+        {
+            if (ModelState.IsValid)
+            {
+                ProductDBContext pdb = new ProductDBContext();
+                pdb.Add(pd);
+                pdb.SaveChanges();
+                return Content("Product Saved");
+            }
+            return View("Index");
+        }
+    }
+}
