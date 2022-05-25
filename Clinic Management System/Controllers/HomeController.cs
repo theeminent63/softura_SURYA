70421806d1cad8480e40c0de8e23de15ba801db3
surya .D <100852000+theeminent63@users.noreply.github.com>
2022-05-25 05:41:51 -0700

    Add files via upload

---
+++ Clinic Management System/Controllers/HomeController.cs
@@ -0,0 +1,148 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using CMSProj.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using CMSProj.DAL;
+
+namespace CMSProj.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult main()
+        {
+            return View();
+        }
+        public IActionResult Homepage()
+        {
+            return View();
+        }
+
+        public IActionResult AddDoctor()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult AddDoc(AddDoctor N)
+        {
+            ClassDAL cobj = new ClassDAL();
+            int result = cobj.DoctorAd(N);
+            if(result ==1)
+            {
+                return RedirectToAction("main");
+            }
+            else
+            {
+                return View("AddDoctor");
+            }
+        }
+        public IActionResult AddPatient()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult AddPat(AddPatient N)
+        {
+            ClassDAL cobj = new ClassDAL();
+            int result = cobj.PatientAd(N);
+            if(result == 1)
+            {
+                return RedirectToAction("main");
+            }
+            else
+            {
+                return View();
+            }
+        }
+        public IActionResult AddApointment()
+        {
+            return View();
+        }
+        [HttpPost]
+
+        public IActionResult Adapo(AddApointment N)
+        {
+            ClassDAL cobj = new ClassDAL();
+            int result = cobj.AppointmentAd(N);
+            if (result == 1)
+            {
+                return RedirectToAction("main");
+            }
+            else
+            {
+                return View();
+            }
+        }
+        public IActionResult CheckAvailabilty()
+        {
+            ClassDAL cobj = new ClassDAL();
+            List<AddDoctor> DoctorList = new List<AddDoctor>();
+            DoctorList = cobj.CheckDoctor();
+            return View(DoctorList);
+        }
+        public IActionResult Delete()
+        {
+            return View();
+        }
+        public IActionResult CancelApointment()
+        {
+            ClassDAL cobj = new ClassDAL();
+            List<AddApointment> patientList = new List<AddApointment>();
+            patientList = cobj.cancelapointment();
+            return View(patientList);
+        }
+        public IActionResult Deleting(AddApointment N)
+        {
+            int result;
+            int PatientID = N.PatientID;
+            ClassDAL cobj = new ClassDAL();
+            result = cobj.DeleteData(N);
+            return RedirectToAction("CancelApointment", N);
+        }
+      
+        public IActionResult Validate(login us)
+        {
+            if(ModelState.IsValid)
+            {
+                ClassDAL pobj = new ClassDAL();
+                int result = pobj.CheckUse(us);
+                if(result==1)
+                {
+                    return View("main");
+                }
+                else
+                {
+                    return Content("Enter valid Username & Password");
+                }
+            }
+            else
+            {
+                return View("Index");
+            }
+        }
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Clinic Management System/DAL/ClassDAL.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using System.IO;
+using System.Data.SqlClient;
+using CMSProj.Models;
+using System.Data;
+
+namespace CMSProj.DAL
+{
+    public class ClassDAL
+    {
+        public string cnn = "";
+        public ClassDAL()
+        {
+            var builder = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory()).AddJsonFile("appsettings.json").Build();
+            cnn = builder.GetSection("ConnectionStrings:con").Value;
+        }
+
+        public int DoctorAd(AddDoctor doc)
+        {
+            int result;
+            SqlConnection con = new SqlConnection(cnn);
+            SqlCommand cmd = new SqlCommand("SPadddoc", con);
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.AddWithValue("@FName", doc.FirstName);
+            cmd.Parameters.AddWithValue("@LName", doc.LastName);
+            cmd.Parameters.AddWithValue("@sex ", doc.Sex);
+            cmd.Parameters.AddWithValue("@spec", doc.Specialization);
+            cmd.Parameters.AddWithValue("@visitingHrs", doc.VisitingHours);
+            con.Open();
+            result = cmd.ExecuteNonQuery();
+            con.Close();
+            return result;
+        }
+
+        public int PatientAd(AddPatient pat)
+        {
+
+            int result;
+            SqlConnection con = new SqlConnection(cnn);
+            SqlCommand cmd = new SqlCommand("SPaddpatient", con);
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.AddWithValue("@PFname", pat.PFirstName);
+            cmd.Parameters.AddWithValue("@PLname", pat.PLastName);
+            cmd.Parameters.AddWithValue("@sex", pat.Sex);
+            cmd.Parameters.AddWithValue("@age", pat.Age);
+            cmd.Parameters.AddWithValue("@DOB", pat.DateOfBirth);
+            con.Open();
+            result = cmd.ExecuteNonQuery();
+            con.Close();
+            return result;
+        }
+
+        public int AppointmentAd(AddApointment apo)
+        {
+            int result;
+            SqlConnection con = new SqlConnection(cnn);
+            SqlCommand cmd = new SqlCommand("SPapp", con);
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.AddWithValue("@PID", apo.PatientID);
+            cmd.Parameters.AddWithValue("@spec", apo.Sepcialization);
+            cmd.Parameters.AddWithValue("@doc", apo.Doctor);
+            cmd.Parameters.AddWithValue("@vd", apo.VisitDate);
+            cmd.Parameters.AddWithValue("@aptime", apo.AppointmentTime);
+            con.Open();
+            result = cmd.ExecuteNonQuery();
+            con.Close();
+            return result;
+        }
+        public int DeleteData(AddApointment can)
+        {
+            SqlConnection con = new SqlConnection(cnn);
+            SqlCommand cmd = new SqlCommand("cancelapointment", con);
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.AddWithValue("@patientid", can.PatientID);
+            con.Open();
+            int result = cmd.ExecuteNonQuery();
+            con.Close();
+            return result;
+        }
+        public List<AddApointment> cancelapointment()
+        {
+            List<AddApointment> listPatient = new List<AddApointment>();
+            using (SqlConnection con = new SqlConnection(cnn))
+            {
+                using (SqlCommand cmd = new SqlCommand("display", con))
+                {
+                    if (con.State == ConnectionState.Closed)
+                        con.Open();
+                    IDataReader reader = cmd.ExecuteReader();
+                    while (reader.Read())
+                    {
+                        listPatient.Add(new AddApointment()
+                        {
+                            PatientID = int.Parse(reader["PatientID"].ToString()),
+                            Sepcialization = reader["Sepcialization"].ToString(),
+                            Doctor = reader["Doctor"].ToString(),
+                            VisitDate = reader["VisitDate"].ToString(),
+                            AppointmentTime = reader["AppointmentTime"].ToString(),
+                        });
+                    }
+                }
+            }
+            return listPatient;
+        }
+        public List<AddDoctor> CheckDoctor()
+        {
+            List<AddDoctor> listPatient = new List<AddDoctor>();
+            using (SqlConnection con = new SqlConnection(cnn))
+            {
+                using (SqlCommand cmd = new SqlCommand("disp", con))
+                {
+                    if (con.State == ConnectionState.Closed)
+                        con.Open();
+                    IDataReader reader = cmd.ExecuteReader();
+                    while (reader.Read())
+                    {
+                        listPatient.Add(new AddDoctor()
+                        {
+                            FirstName = reader["FirstName"].ToString(),
+                            Sex = reader["Sex"].ToString(),
+                            Specialization = reader["Specialization"].ToString(),
+                            VisitingHours = reader["VisitingHours"].ToString()
+                        });
+                    }
+                }
+            }
+            return listPatient;
+        }
+        public int CheckUse(login us)
+        {
+            
+            SqlConnection con = new SqlConnection(cnn);
+            SqlCommand cmd = new SqlCommand("loginUser", con);
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.AddWithValue("@UName", us.UserName);
+            cmd.Parameters.AddWithValue("@Pword", us.Password);
+            con.Open();
+            IDataReader read = cmd.ExecuteReader();
+            if (read.Read())
+            {
+                return (1);
+            }
+            con.Close();
+            return (0);
+        }
+    }
+}
